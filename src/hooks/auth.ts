359b4990639af9cef9d474b83d4aa29685fdd364
bylel <bylelbrmm@gmail.com>
2026-04-27 15:08:08 +0200

    feat: implement signin function for user authentication

--- src/hooks/auth.ts
+++ src/hooks/auth.ts
@@ -1,6 +1,26 @@
 import { useState } from 'react';
 import { API_URL } from '../utils/constants';
 
+export const signin = async (username: string, password: string) => {
+    const response = await fetch(
+        `${API_URL}/auth/login`,
+        {
+            method: 'POST',
+            body: JSON.stringify(
+                {username, password}
+            )
+        }
+    )
+
+    if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem("authToken", data.token);
+    } else {
+        alert("login/password incorrect")
+    }
+}
+
+
 type RegisterPayload = {
 	username: string;
 	password: string;
